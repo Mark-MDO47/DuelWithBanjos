2907a768dfc1cb0d3b3cdf1ed4f313cca8b5ebd6
Mark-MDO47 <mdo@ieee.org>
2025-03-30 11:52:02 -0700

    update SoundNum.h

---
+++ code/DuelWithBanjos/SoundNum.h
@@ -0,0 +1,19 @@
+// DuelWithBanjos
+// Author: https://github.com/Mark-MDO47/
+//
+
+#ifndef SOUNDNUM_h
+
+#define SOUNDNUM_h
+
+#define SOUNDNUM_DuelingBanjos       1
+#define SOUNDNUM_silence             2
+#define SOUNDNUM_DeckTheDuelingHalls 3
+#define SOUNDNUM_WhatChildIsThis     4
+#define SOUNDNUM_GodRestYe           5
+#define SOUNDNUM_OLittleTownOf       6
+#define SOUNDNUM_GoodKingWenceslas   7
+
+#define SOUNDNUM_INVALID 254 // invalid soundnum
+
+#endif // SOUNDNUM_h
